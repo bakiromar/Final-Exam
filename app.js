bdc5f4e2273889fa58767929df4e47fddabd207a
Bakr Al-Humaimidi <bakir_omer66@yahoo.com>
2018-12-11 02:50:59 -0800

    Changed Home page

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-    response.send('<h1>Welcome to My Page!</h1>' +
+    response.send('<h1>Welcome Bakr!</h1>' +
         '<a href="/about.html">Image</a>' +
         '<p></p>' +
         '<a href="/weather">Weather</a>');
